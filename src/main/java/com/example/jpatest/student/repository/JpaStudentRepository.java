6d1fde38bc621f0458369111ce3c963ca9458b64
bsj001 <920757938@qq.com>
2024-04-23 22:40:33 +0800

    添加自定义sql查询

--- src/main/java/com/example/jpatest/student/repository/JpaStudentRepository.java
+++ src/main/java/com/example/jpatest/student/repository/JpaStudentRepository.java
@@ -27,4 +27,38 @@ public interface JpaStudentRepository extends JpaRepository<Student, Integer> {
     //@Query("select s.name as name ,sc.school_name as schoolName from Student s left join School sc on s.school_id = sc.id where (:ids is null or s.id in :ids) and (:name is null or s.name = :name)")
     @Query("select s.name as name ,sc.school_name as schoolName from Student s left join School sc on s.school_id = sc.id where (:ids is null or s.id in :ids) and (:name is null or sc.school_name = :name)")
     Page<StudentAndSchoolInfo> listStudentAndSchool4(@Param("ids") List<Integer> ids , @Param("name") String name, @Param("page")Pageable pageable);
+    
+    //repository自定义查询
+    //@Query(value = "SELECT cr.project_number as projectNumber,cr.file_number as fileNumber,cr.role_id as projectRoleId," +
+    //        "r.project_role as projectRole,r.responsibility_text as responsibilityText," +
+    //        "GROUP_CONCAT(cr.contacts_id) as contactsId,GROUP_CONCAT(c.user_full_name)as userFullName " +
+    //        "FROM project_role_contacts_relation cr left JOIN contacts_info c on cr.contacts_id=c.id " +
+    //        "LEFT JOIN project_role r on cr.role_id=r.id WHERE cr.project_number=?1 AND cr.file_number=?2 " +
+    //        "GROUP BY cr.role_id",nativeQuery = true)
+    //List<Object[]> roleAndDutyVoList(String projectNumber, String fileNumber);
+    
+    //2,接收类
+    //@Data
+    //@AllArgsConstructor  //（有参构造）
+    //@NoArgsConstructor   //（无参构造）
+    //public class RoleAndDutyVo {
+    //
+    //    private String projectNumber;
+    //    private String fileNumber;
+    //    private String projectRoleId;
+    //    private String projectRole;
+    //    private String responsibilityText;
+    //    private String contactsId;
+    //    private String userFullName;
+    //
+    //}
+    
+    //3.把List<Object[]>转化成List<RoleAndDutyVo>
+    
+    //4,实际业务实现
+    //public List<RoleAndDutyVo> roleContactsList(String projectNumber, String fileNumber) {
+    //    List<Object[]> objects = projectRoleContactsRepository.roleAndDutyVoList(projectNumber, fileNumber);
+    //    List<RoleAndDutyVo> vos = EntityUtils.castEntity(objects, RoleAndDutyVo.class, new RoleAndDutyVo());
+    //
+    //}
 }
